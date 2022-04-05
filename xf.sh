8af00cda13c24ac5b15aed67af53a1413865f63c
svfbrgrdnt <91479928+svfbrgrdnt@users.noreply.github.com>
2022-04-05 08:28:37 +0000

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "58b66463-82cb-4d01-b889-901864bf3a62",
                         "alterId": 0
                     }
                 ],
